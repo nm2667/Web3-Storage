a860e12be0e637dc97936614acca1c1ee9311acd
sarthakk24 <sarthak.sachdeva.73@gmail.com>
2023-02-18 22:50:53 +0530

    :construction: add input file option

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
+import FileInput from 'components/shared/fileInput';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,7 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p className="text-5xl">sarthak</p>
+      <div className="h-screen w-screen flex justify-center items-center">
+        <FileInput />
+      </div>
     </>
   );
 }
